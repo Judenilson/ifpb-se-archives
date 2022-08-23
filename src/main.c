248147c76f01df0a241e286143227905fd79aaee
antonio357 <carlosalbuquerque359@gmail.com>
2022-08-22 23:38:27 -0300

    volta

--- src/main.c
+++ src/main.c
@@ -471,6 +471,14 @@ void app_main(void)
 	gpio_set_level(LIGHT_PIN, 0);
 	light_state = 0;
 
+    // Initialize NVS caso necessite guardar alguma config na flash.
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
     ESP_LOGI(TAGM, "ESP_WIFI_MODE_STA");
     wifi_init_sta();        
 	setup_server();
