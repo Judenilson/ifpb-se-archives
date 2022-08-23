fdb95437bc8d7a9d49d0c45a93321444c5ac8a42
antonio357 <carlosalbuquerque359@gmail.com>
2022-08-23 10:32:03 -0300

    comentário

--- src/main.c
+++ src/main.c
@@ -505,7 +505,7 @@ void app_main(void)
 	gpio_set_level(LIGHT_PIN, 0);
 	light_state = 0;
 
-    Initialize NVS caso necessite guardar alguma config na flash.
+    // Initialize NVS caso necessite guardar alguma config na flash.
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
